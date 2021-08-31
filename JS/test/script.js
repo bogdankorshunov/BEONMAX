f883a3912c29017d4a56f31058c8c219ae8138ba
bogdankorshunov <korshunov.oren@gmail.com>
2021-08-31 03:39:26 -0700

    task 12 Promise

--- JS/test/script.js
+++ JS/test/script.js
@@ -1,9 +1,31 @@
-function log(...test) {
-  for (const iterator of test) {
-    console.log(iterator)
-  }
-}
+let prom = new Promise((resolve, reject) => {
+  console.log('timer 2000 started')
+  setTimeout(()=>{
+    let t = 2
+    console.log('timer ended')
+    if(t == 2) {
 
-let arr=[1,2,3,4,5]
+      resolve({timer: 'ok'})
+    } else {
+      reject({timer: 'ne ok'})
+    }
+  }, 2000)
+})
+let prom2 = new Promise((resolve, reject) => {
+  console.log('timer 3000 started')
+  setTimeout(()=>{
+    let t = 3
+    console.log('timer ended')
+    if(t == 2) {
 
-log(...arr)
+      resolve({timer: 'ok'})
+    } else {
+      reject({timer: 'ne ok'})
+    }
+  }, 3000)
+})
+// prom.then(data => console.log(data.timer)).catch(data => console.log(data.timer))
+
+Promise.all([prom, prom2]).then(()=>{
+  console.log('ok')
+})
